98a04c98c2bb48bf4a40619590201342d5996bbd
Wesley Silva <wesley.bueno.silva.wbs@gmail.com>
2020-10-30 09:43:18 -0300

    Exercicio_hello

---
+++ Spring/Aula/dados2/src/main/java/com/example/demo/ServicoController.java
@@ -0,0 +1,42 @@
+package com.example.demo;
+
+
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+//ponto no endereço do site
+@RequestMapping("/hello")
+@RestController
+public class ServicoController {
+
+	//MVC --- model(tabelas)
+	//MVC --- view(Front-end)
+	//MVC --- controller(cerebro, endereçamento de páginas)
+	//JPA é a interface
+	
+	@GetMapping("/get1")
+	public String hello1() {
+		return "Hey Get1 Hello World";
+		
+	}
+	
+	@GetMapping("/get2")
+	public String hello2() {
+		return "Hey Get2 Hello World";
+			
+	}
+	
+	//injeção
+			@Autowired
+			private ServicoRepository repository;
+			
+			@GetMapping ("/servicos")
+			public List<ServicoModel> pegarTodos() {		
+				return repository.findAll();
+			}
+}
